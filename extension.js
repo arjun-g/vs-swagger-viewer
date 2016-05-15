3459d73a3abd6992b6cf4b67e6e8a12c9471973c
Arjun Ganesan <arjun@arjun-g.com>
2016-05-15 17:59:30 +0530

    Using dist code from swagger editor instead of source code to avoid large fiel size.

--- extension.js
+++ extension.js
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
-var shell = require("shelljs");
 var path = require('path');
 
 var ports = {}
--- static/bower_components/sway-worker/README.md
+++
@@ -1,5 +0,0 @@
-# [sway](https://github.com/apigee-127/sway) in a Web Worker
-
-This module is built specifically for [Swagger Editor](https://github.com/swagger-api/swagger-editor).
-
-MIT
